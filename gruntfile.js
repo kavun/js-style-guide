b80c4c3d65f99af6e486fb2306246a43fdfac049
Kevin Reed <kevin.a.reed@gmail.com>
2013-09-17 23:16:38 -0400

    rename dist pdf

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,32 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		markdownpdf: {
 			options: {
 				cssPath: 'pdf.css'
 			},
 			files: {
-				src: "*.md",
-				dest: ""
+				src: "README.md",
+				dest: 'dist'
 			}
+		},
+		rename: {
+		  main: {
+		    files: [
+		        {
+		        	src: ['dist/README.pdf'], 
+		        	dest: 'dist/<%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %>.pdf'
+		        },
+	        ]
+		  }
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-markdown-pdf');
+	grunt.loadNpmTasks('grunt-contrib-rename');
 
 	// Default task(s).
-	grunt.registerTask('pdf', ['markdownpdf']);
+	grunt.registerTask('pdf', ['markdownpdf', 'rename']);
 
 };
